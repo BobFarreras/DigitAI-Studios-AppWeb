8f2129b7cf1c6d7a3fe6ad0b2cec7ad5cbe787dc
BobFarreras <143645540+BobFarreras@users.noreply.github.com>
2025-11-26 17:29:56 +0100

    Em colcoat mp4 perque isno no agafava videos
    Em colcoat mp4 perque isno no agafava videos

--- src/proxy.ts
+++ src/proxy.ts
@@ -15,6 +15,5 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|mp4|webm)$).*)',],
 };
